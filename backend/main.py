06ab9482aec9549f3a9d9922aff33591de059916
Alejo <alejito23001@gmail.com>
2024-10-12 03:13:26 -0500

    feat(api): add cors

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from os import getenv
 
 import uvicorn
-from fastapi import FastAPI
-
 from app.config.database_config import init_db
 from app.routers import daily_stats_router, newcast_router, upload_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 stage = getenv("STAGE", "dev")
 app = FastAPI()
@@ -12,6 +12,14 @@ init_db()
 app.include_router(upload_router.router)
 app.include_router(newcast_router.router)
 app.include_router(daily_stats_router.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
 
 if __name__ == "__main__":
     if "prod" in stage:
